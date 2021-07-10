660d3f5cd1ebc5ef8729cb2ab8fd70a211a2ac71
frappe <frappe@ubuntu.vm>
2021-07-10 20:09:26 +0300

    Modified in tenders #Ansi

---
+++ tender_management/tender_management/doctype/tender_category/tender_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Michael Karamanolis and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tender Category', {
+	// refresh: function(frm) {
+
+	// }
+});
