b09a3d03c4f0a21c4064b2d73c8f368f1af8a974
ansiabdo <ansi.abdooh@gmail.com>
2021-07-10 16:37:12 +0300

    Added tender type #Ansi

---
+++ tender_management/tender_management/doctype/tender_type/tender_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Michael Karamanolis and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Tender Type', {
+	// refresh: function(frm) {
+
+	// }
+});
