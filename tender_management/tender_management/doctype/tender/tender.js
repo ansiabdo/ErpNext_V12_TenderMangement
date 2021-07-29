fdb126ba1f697a8b76f2a2f579b297388206b55f
frappe <frappe@ubuntu.vm>
2021-07-29 17:39:34 +0300

    Modified in tenders #Ansi

--- tender_management/tender_management/doctype/tender/tender.js
+++ tender_management/tender_management/doctype/tender/tender.js
@@ -2,7 +2,10 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Tender', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('New Bank Guarantee'),function(){
+			frappe.msgprint(frm.doc.email);  
+			}, __("Create"));
+			
+	}		
 });
